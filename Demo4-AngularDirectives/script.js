aeb0a2478538c018303d0dd553520d6771de4923
Joe Bowen <joe.b.bowen@gmail.com>
2015-11-10 21:27:03 +0000

    Updated the demos with angularjs functionality.

---
+++ Demo4-AngularDirectives/script.js
@@ -0,0 +1,58 @@
+var app = angular.module("myGroceryCart", []);
+
+app.directive('ngGroceryCart', function() {
+  return {
+    restrict: 'A',
+    templateUrl: 'cart-template.html',
+    scope: {},
+    controller: ['$scope', function($scope) {
+            $scope.items=[
+                { title: 'web development', price: 200},
+                { title: 'web design', price: 250},
+                { title: 'photography', price: 100},
+                { title: 'coffee drinking', price: 10}];
+            $scope.shopping_cart=[];
+            
+            $scope.add_to_cart=function(){
+                for (var i = $scope.items.length - 1; i >= 0; i--){
+                    var s = $scope.items[i];
+                    if(s.selected)
+                    {
+                        $scope.shopping_cart = $scope.shopping_cart.concat([s]);
+                        
+                        var index = $scope.items.indexOf(s);
+                        
+                        if (index > -1) {
+                            $scope.items.splice(index, 1);
+                        }
+                    }
+                }
+            };
+            
+            $scope.remove_from_cart=function(){
+                for (var i = $scope.shopping_cart.length - 1; i >= 0; i--){
+                    var s = $scope.shopping_cart[i];
+                    if(s.selected)
+                    {
+                        $scope.items = $scope.items.concat([s]);
+                        
+                        var index = $scope.shopping_cart.indexOf(s);
+                        
+                        if (index > -1) {
+                            $scope.shopping_cart.splice(index, 1);
+                        }
+                    }
+                }
+            };
+            
+            $scope.total=function(){
+                var t = 0;
+                angular.forEach($scope.shopping_cart, function(s){
+                    t+=s.price;
+                });
+                return t;
+            };
+        }],
+        
+    }
+});
